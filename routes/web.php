494681df8709a8a9c07aaf989df7a7a351d31540
jarji <100996174+jarjia@users.noreply.github.com>
2023-04-18 17:16:22 +0400

    feat: adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ForgotPasswordController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +19,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/signup');
 });
+
+Route::group(['controller' => RegisterController::class], function () {
+    Route::get('/signup', 'create')->name('signup.create');
+    Route::post('/signup', 'store')->name('signup.store');
+    Route::get('/signup/verify', 'verify')->middleware('auth')->name('signup.verify');
+    Route::get('/email/verify', 'verifying')->middleware('auth')->name('verification.notice');
+});
+
+Route::group(['controller' => SessionsController::class], function () {
+    Route::get('login', 'create')->middleware('guest')->name('login.create');
+    Route::post('login', 'store')->middleware('guest')->name('login.store');
+    Route::get('logout', 'destroy')->middleware('auth')->name('logout.destroy');
+});
+
+Route::group(['controller' => ForgotPasswordController::class, 'middleware' => 'guest', 'prefix' => '/forgot-password'], function () {
+    Route::get('/', 'email')->name('recover.email');
+    Route::post('/', 'send')->name('recover.send');
+    Route::get('/recover/verify', 'message')->name('recover.password.message');
+    Route::get('/recover/{token}', 'password')->name('recover.password.create');
+    Route::post('/reset-password', 'reset')->name('recover.password.reset');
+});
+
+Route::group([], function () {
+    Route::view('email-verified', 'auth.verified', 
+        ['message' => 'Your account is confirmed, you can sign in', 'redirect' => 'logout.destroy']
+    )->middleware(['auth', 'verified'])->name('email.verified');
+
+    Route::view('recover-verified', 'auth.verified', 
+        ['message' => 'Your password has been updated successfully', 'redirect' => 'login.create']
+    )->middleware('guest')->name('recover.verified');
+});
+
+Route::view('/test', 'auth.mail.verify-notice');
+
+Route::view('/dashboard', 'dashboard.dashboard')->name('dashboard');
