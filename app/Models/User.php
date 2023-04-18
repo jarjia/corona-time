e3d39acffe5f18ba485a705c667357542b00c2d9
jarji <100996174+jarjia@users.noreply.github.com>
2023-04-18 17:18:20 +0400

    feat: adding hashing and reset email

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\ResetPasswordNotification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Auth\Passwords\CanResetPassword as CanResetPasswordTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, CanResetPasswordTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +26,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'remember_token'
     ];
 
     /**
@@ -32,7 +38,21 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+    
+    /*
+     * This function hashes user's password
+    */
+    public function setPasswordAttribute($password) {
+        $this->attributes['password'] = bcrypt($password);
+    }
 
+    /*
+     * This function helps us to send password reset notification
+     * to users mail
+    */
+    public function sendPasswordResetNotification($token) { 
+        $this->notify(new ResetPasswordNotification($token));
+    }
     /**
      * The attributes that should be cast.
      *
