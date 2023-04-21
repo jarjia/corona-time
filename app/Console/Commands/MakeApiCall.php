f577a14f5310b92cad884732bc50f2f183520e9a
jarji <100996174+jarjia@users.noreply.github.com>
2023-04-21 18:11:44 +0400

    feat: adding command that should run every 24 hours

---
+++ app/Console/Commands/MakeApiCall.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\CountryCodes;
+use App\Models\CountryStatistics;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class MakeApiCall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'corona-time:make-api-call';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command makes api call every 24 hour.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $getresponse = Http::get('https://devtest.ge/countries');
+
+        $countries = $getresponse->json();
+
+        foreach ($countries as $country) {
+            CountryCodes::updateOrCreate(
+                ['code' => $country['code']],
+                [
+                    'name' => [
+                        'en' => $country['name']['en'],
+                        'ka' => $country['name']['ka'],
+                    ],
+                ]
+            );
+            $postResponse = Http::post('https://devtest.ge/get-country-statistics', [
+                'code' => $country['code']
+            ])->json();
+
+            CountryStatistics::updateOrCreate(
+                ['code' => $postResponse['code']],
+                [
+                    'country' => $postResponse['country'],
+                    'new_cases' => $postResponse['confirmed'],
+                    'recovered' => $postResponse['recovered'],
+                    'deaths' => $postResponse['deaths']
+                ]
+            );
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,13 +6,21 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
-{
+{    
+    /**
+    * The Artisan commands provided by your application.
+    *
+    * @var array
+    */
+   protected $commands = [
+       Commands\MakeApiCall::class,
+   ];
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('corona-time:make-api-call')->everyMinute();
     }
 
     /**
