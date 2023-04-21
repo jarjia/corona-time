a6e7c63b30477a6e4fdbbdccd7f4279f1e2c9fd0
jarji <100996174+jarjia@users.noreply.github.com>
2023-04-21 20:15:07 +0400

    chore: modifying dashboard pages

--- resources/views/dashboard/dashboard-by-country.blade.php
+++ resources/views/dashboard/dashboard-by-country.blade.php
@@ -1,12 +1,20 @@
 <x-layout-for-dashboard>
     <section class='py-10'>
-        <section class='sm:px-1'>
-            <form method='GET' action='#'>
-                <div class="relative rounded-md">
-                    <input type="email" name="email" id="email" 
-                        class="rounded-[8px] py-4 px-32 block w-auto sm:w-full outline-none sm:border-0 border-[1px] border-[#E6E6E7] pl-10 sm:text-sm sm:leading-5"
+        <section class='sm:px-0'>
+            <form method='GET' action="{{route('dashboard.show')}}">
+                @if (request('sort'))
+                    <input type='hidden' name='sort' value="{{ request('sort') }}"/>
+                @endif
+                @if (request('type'))
+                    <input type='hidden' name='type' value="{{ request('type') }}"/>
+                @endif
+                <div class="relative w-[300px] sm:w-full rounded-md">
+                    <input type="text" name="search" id="search" 
+                        class="form-input rounded-[8px] py-4 block w-full sm:w-full focus:ring-0 sm:border-0 border-[1px] border-[#E6E6E7] pl-10 focus:outline-none"
                         placeholder="Search by country"
+                        value="{{request('search')}}"
                         autocomplete='off'
+                        style="outline:none;"
                     >
                     <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                         <img src="{{asset('images/search.png')}}"/>
@@ -14,71 +22,87 @@
                 </div>
             </form>
         </section>
-        <section class='mt-12 sm:mt-4'>
-            <div class="relative overflow-x-auto">
-                <table class="w-full text-sm sm:text-[10px] text-left">
-                    <thead class="text-xs sm:text-[11px] bg-[#F6F6F7] dark:bg-gray-700 text-black">
-                        <tr>
-                            <th scope="col" class="px-6 py-3 sm:px-2">
-                                Location
-                            </th>
-                            <th scope="col" class="px-6 py-3 sm:px-2">
-                                New cases
-                            </th>
-                            <th scope="col" class="px-6 py-3 sm:px-2">
-                                Deaths
-                            </th>
-                            <th scope="col" class="px-6 py-3 sm:px-2">
-                                Recovered
-                            </th>
+        <div class="container mt-12 sm:mt-4 rounded-[8px] shadow">
+            <table class="text-left w-full">
+                <thead class="bg-[#F6F6F7] font-[900] sm:text-[12px] rounded-t-[8px] flex text-black w-full">
+                    <tr class="flex w-full">
+                        <th class="p-4 sm:p-1 w-1/4 flex sm:text-[10px] items-center sm:gap-0 gap-2">
+                            {{__('dashboard.location')}}
+                            <a href="{{route('dashboard.sortby.location', ['sort' => 'name', 'search' => request('search')])}}" class='py-1 px-1'>
+                                @if ($sort_name === 'name' && $sort_type === 'desc')
+                                    <img src="{{asset('images/arrows/down-colored.png')}}" class='mb-[4px] sm:w-[8px] rotate-180'/>
+                                    <img src="{{asset('images/arrows/down-arrow.png')}}" class='sm:w-[8px]'/>
+                                @else
+                                    <img src="{{asset('images/arrows/up-arrow.png')}}" class='mb-[4px]'/>
+                                    <img src="{{asset('images/arrows/down-colored.png')}}" class='sm:w-[8px]'/>
+                                @endif
+                            </a>
+                        </th>
+                        <th class="p-4 sm:p-1 w-1/4 flex sm:text-[10px] items-center sm:gap-0 gap-2">
+                            <span>{{__('dashboard.new_cases')}}</span>
+                            <a href="{{route('dashboard.sortby.new_cases', ['sort' => 'new_cases', 'search' => request('search')])}}" class='py-1 px-1'>
+                                @if ($sort_name === 'new_cases' && $sort_type === 'desc')
+                                    <img src="{{asset('images/arrows/down-colored.png')}}" class='mb-[4px] min-w-[8px] sm:w-[8px] rotate-180'/>
+                                    <img src="{{asset('images/arrows/down-arrow.png')}}" class='sm:w-[8px] min-w-[8px]'/>
+                                @elseif ($sort_name === 'new_cases' && $sort_type === 'asc')
+                                    <img src="{{asset('images/arrows/up-arrow.png')}}" class='mb-[4px] min-w-[8px] sm:w-[8px]'/>
+                                    <img src="{{asset('images/arrows/down-colored.png')}}" class='sm:w-[8px] min-w-[8px]'/>
+                                @else
+                                    <img src="{{asset('images/arrows/up-arrow.png')}}" class='mb-[4px] min-w-[8px] sm:w-[8px]'/>
+                                    <img src="{{asset('images/arrows/down-arrow.png')}}" class='sm:w-[8px] min-w-[8px]'/>
+                                @endif
+                            </a>
+                        </th>
+                        <th class="p-4 sm:p-1 w-1/4 flex sm:text-[10px] items-center sm:gap-0 gap-2">
+                            {{__('dashboard.deaths')}}
+                            <a href="{{route('dashboard.sortby.deaths', ['sort' => 'deaths', 'search' => request('search')])}}" class='py-1 px-1'>
+                                @if ($sort_name === 'deaths' && $sort_type === 'desc')
+                                    <img src="{{asset('images/arrows/down-colored.png')}}" class='mb-[4px] sm:w-[8px] rotate-180'/>
+                                    <img src="{{asset('images/arrows/down-arrow.png')}}" class='sm:w-[8px]'/>
+                                @elseif ($sort_name === 'deaths' && $sort_type === 'asc')
+                                    <img src="{{asset('images/arrows/up-arrow.png')}}" class='mb-[4px] sm:w-[8px]'/>
+                                    <img src="{{asset('images/arrows/down-colored.png')}}" class='sm:w-[8px]'/>
+                                @else
+                                    <img src="{{asset('images/arrows/up-arrow.png')}}" class='mb-[4px] sm:w-[8px]'/>
+                                    <img src="{{asset('images/arrows/down-arrow.png')}}" class='sm:w-[8px]'/>
+                                @endif
+                            </a>
+                        </th>
+                        <th class="p-4 sm:p-1 w-1/4 flex sm:text-[10px] items-center sm:gap-0 gap-2">
+                            <span class='truncate'>{{__('dashboard.recovered')}}</span>
+                            <a href="{{route('dashboard.sortby.recovered', ['sort' => 'recovered', 'search' => request('search')])}}" class='py-1 px-1'>
+                                @if ($sort_name === 'recovered' && $sort_type === 'desc')
+                                    <img src="{{asset('images/arrows/down-colored.png')}}" class='mb-[4px] sm:w-[8px] rotate-180'/>
+                                    <img src="{{asset('images/arrows/down-arrow.png')}}" class='sm:w-[8px]'/>
+                                @elseif ($sort_name === 'recovered' && $sort_type === 'asc')
+                                    <img src="{{asset('images/arrows/up-arrow.png')}}" class='mb-[4px] sm:w-[8px]'/>
+                                    <img src="{{asset('images/arrows/down-colored.png')}}" class='sm:w-[8px]'/>
+                                @else
+                                    <img src="{{asset('images/arrows/up-arrow.png')}}" class='mb-[4px] sm:w-[8px]'/>
+                                    <img src="{{asset('images/arrows/down-arrow.png')}}" class='sm:w-[8px]'/>
+                                @endif
+                            </a>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="bg-grey-light font-[500] max-h-[604px] sm:text-[11px] flex flex-col scrollbar items-center justify-between overflow-y-scroll w-full">
+                    <tr class="flex w-full mb-4">
+                        <td class="p-3 sm:p-1 w-1/4 text-black">{{__('dashboard.world_wide')}}</td>
+                        <td class="p-3 sm:p-1 w-1/4 text-black">{{ number_format($new_cases, 0, ',', ',') }}</td>
+                        <td class="p-3 sm:p-1 w-1/4 text-black">{{ number_format($deaths, 0, ',', ',') }}</td>
+                        <td class="p-3 sm:p-1 w-1/4 text-black">{{ number_format($recovered, 0, ',', ',') }}</td>
+                    </tr>
+                    @foreach ($countries as $country)
+                        <tr class="flex w-full mb-4">
+                            <td class="p-3 sm:p-1 w-1/4 text-black">{{$country->countryCodes->name}}</td>
+                            <td class="p-3 sm:p-1 w-1/4 text-black">{!! $country->new_cases < 1 ? '<strong>-</strong>' : $country->new_cases !!}</td>
+                            <td class="p-3 sm:p-1 w-1/4 text-black">{!! $country->deaths < 1 ? '<strong>-</strong>' : $country->deaths !!}</td>
+                            <td class="p-3 sm:p-1 w-1/4 text-black">{!! $country->recovered < 1 ? '<strong>-</strong>' : $country->recovered !!}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="bg-white border-b border-[#F6F6F7] dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 sm:px-2 font-medium text-black whitespace-nowrap">
-                                Worldwide
-                            </th>
-                            <td class="px-6 py-4 sm:px-2">
-                                9,704,000
-                            </td>
-                            <td class="px-6 py-4 sm:px-2">
-                                66,591
-                            </td>
-                            <td class="px-6 py-4 sm:px-2">
-                                5,803,905
-                            </td>
-                        </tr>
-                        <tr class="bg-white border-b border-[#F6F6F7] dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 sm:px-2 font-medium text-black whitespace-nowrap">
-                                United States of America
-                            </th>
-                            <td class="px-6 py-4 sm:px-2">
-                                66,591
-                            </td>
-                            <td class="px-6 py-4 sm:px-2">
-                                -
-                            </td>
-                            <td class="px-6 py-4 sm:px-2">
-                                66,591
-                            </td>
-                        </tr>
-                        <tr class="bg-white border-b border-[#F6F6F7] dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 sm:px-2 font-medium text-black whitespace-nowrap">
-                                Uruguay
-                            </th>
-                            <td class="px-6 py-4 sm:px-2">
-                                66,591
-                            </td>
-                            <td class="px-6 py-4 sm:px-2">
-                                66,591
-                            </td>
-                            <td class="px-6 py-4 sm:px-2">
-                                66,591
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </section>
+                    @endforeach
+                    
+                </tbody>
+            </table>
+        </div>
     </section>
 </x-layout-for-dashboard>
