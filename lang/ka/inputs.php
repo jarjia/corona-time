e5796e5f68a3ce767900dc7bbb8b5f86bbe95ca9
jarji <100996174+jarjia@users.noreply.github.com>
2023-04-23 12:25:57 +0400

    feat: adding translations for ka folder

---
+++ lang/ka/inputs.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'email' => 'ელ-ფოსტა',
+    'email_placeholder' => 'შეიყვანეთ ელ-ფოსტა',
+    'password' => 'პაროლი',
+    'password_placeholder' => 'შეიყვანეთ პაროლი',
+    'username' => 'მომხმარებლის სახელი',
+    'username_placeholder' => 'შეიყვანეთ მომხმარებლის სახელი',
+    'confirm_password' => 'გაიმეორეთ პაროლი',
+    'confirm_password_placeholder' => 'გაიმეორეთ პაროლი',
+    'new_password' => 'ახალი პაროლი',
+    'new_password_placeholder' => 'შეიყვანეთ ახალი პაროლი',
+    'checkbox' => 'მოწყობილობის დამახსოვრება',
+    'search' => 'მოძებნე ქვეყნით'
+];
