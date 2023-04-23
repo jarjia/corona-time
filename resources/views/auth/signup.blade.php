2c726b8c659eeb789153ab1627d53c1bd00f9201
jarji <100996174+jarjia@users.noreply.github.com>
2023-04-23 12:29:23 +0400

    feat: adding static translations throughout the app

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,21 +1,21 @@
 <x-layout-for-auth>
     <div class='mb-6'>
-        <h2 class='text-xl mb-2 font-bold sm:text-2xl'>Welcome to Coronatime</h2>
-        <h3 class='text-md tracking-wide text-gray sm:text-lg'>Please enter required info to sign up</h3>
+        <h2 class='text-xl mb-2 font-bold sm:text-2xl'>{{__('forms.welcome_to_corona_time')}}</h2>
+        <h3 class='text-md tracking-wide text-gray sm:text-lg'>{{__('forms.register_desc')}}</h3>
     </div>
     <form method='POST' action="{{route('signup.store')}}" class='pr-16 sm:pr-2 mb-2'>
         @csrf
 
-        <x-input :name="'name'" :type="'text'" :label="'Username'" placeholder='Enter unique username'/>
-        <x-input :name="'email'" :type="'email'" :label="'Email'" placeholder='Enter your email'/>
-        <x-input :name="'password'" :type="'password'" :label="'Password'" placeholder='Fill in password'/>
-        <x-input :name="'password_confirmation'" :type="'password'" :label="'Repeat Password'" placeholder='Repeat password'/>
+        <x-input :name="'name'" :type="'text'" :label="__('inputs.username')" :placeholder="__('inputs.username_placeholder')"/>
+        <x-input :name="'email'" :type="'email'" :label="__('inputs.email')" :placeholder="__('inputs.email_placeholder')"/>
+        <x-input :name="'password'" :type="'password'" :label="__('inputs.password')" :placeholder="__('inputs.password_placeholder')"/>
+        <x-input :name="'password_confirmation'" :type="'password'" :label="__('inputs.confirm_password')" :placeholder="__('inputs.confirm_password_placeholder')"/>
 
         <button type='submit' class='mt-4 w-full bg-sign-up-btn tracking-wide text-white py-4 rounded-md'>
-            SIGN UP
+            {{__('forms.sign_up')}}
         </button>
     </form>
     <div class='text-left sm:text-center sm:py-2'>
-        <small class='text-gray text-primary-font sm:text-[14px]'>Already have an account? <a href="{{route('login.create')}}" class='text-black font-bold'>Log in</a></small>
+        <small class='text-gray text-primary-font sm:text-[14px]'>{{__('forms.have_acc')}} <a href="{{route('login.create')}}" class='text-black font-bold'>{{__('forms.log_in_link')}}</a></small>
     </div>
 </x-layout-for-auth>
--- resources/views/auth/verifyPassword/recover-password.blade.php
+++ resources/views/auth/verifyPassword/recover-password.blade.php
@@ -1,7 +1,7 @@
 <x-layout-for-confirmations>
     <div class='w-1/4 sm:w-full'>
         <div class='mb-8 sm:mb-4 text-center relative sm:bottom-[50px]'>
-            <h2 class='text-2xl mb-2 font-bold'>Reset Password</h2>
+            <h2 class='text-2xl mb-2 font-bold'>{{__('forms.reser_password')}}</h2>
         </div>
         <form method='POST' action="{{route('recover.password.reset')}}" class='block sm:flex sm:flex-col sm:justify-between sm:h-full'>
         @csrf
@@ -9,12 +9,12 @@
         <div class='relative sm:bottom-[50px]'>
             <input type='hidden' name='email' value="{{$email}}"/>
             <input type='hidden' name='token' value="{{$token}}"/>
-            <x-input :name="'password'" :type="'password'" :label="'New Password'" placeholder='Enter New Password'/>
-            <x-input :name="'password_confirmation'" :type="'password'" :label="'Repeat Password'" placeholder='Repeat password'/>
+            <x-input :name="'password'" :type="'password'" :label="__('inputs.new_password')" :placeholder="__('inputs.new_password_placeholder')"/>
+            <x-input :name="'password_confirmation'" :type="'password'" :label="__('inputs.confirm_password')" :placeholder="__('inputs.confirm_password_placeholder')"/>
         </div>
 
         <button type='submit' class='mt-2 w-full bg-sign-up-btn tracking-wide text-white py-4 rounded-md'>
-            SAVE CHANGES
+            {{__('forms.save_changes')}}
         </button>
         </form>
     </div>
