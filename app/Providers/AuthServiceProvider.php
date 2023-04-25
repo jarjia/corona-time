6c685d0efee38a3e1df50ba7d3dbfc2a94207c27
jarji <100996174+jarjia@users.noreply.github.com>
2023-04-25 20:02:36 +0400

    feat: implementing laravels email verification logic

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,15 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verify Email Address')
+                ->view('auth.mail.verify-notice', [
+                    'verifyUrl' => $url
+                ])->attach(public_path('/images/email-verify.png'), [
+                    'as' => 'email-verify.png',
+                    'mime' => 'email-verify/png',
+                ]);
+        });
     }
 }
