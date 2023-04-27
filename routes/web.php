a00f9f940d1a14cd6a0b17d151e8e3fe4de20b56
jarji <100996174+jarjia@users.noreply.github.com>
2023-04-27 17:39:45 +0400

    fix: removing unneccessary group

--- routes/web.php
+++ routes/web.php
@@ -21,35 +21,33 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/signup');
 
 Route::group(['controller' => RegisterController::class], function () {
-    Route::get('/signup', 'create')->name('signup.create');
+    Route::view('/signup', 'auth.signup')->name('signup.create');
     Route::post('/signup', 'store')->name('signup.store');
     Route::get('/email/verify/{id}/{hash}', 'verify')->middleware(['auth', 'signed'])->name('verification.verify');
     Route::get('/email/verify', 'verifying')->middleware('auth')->name('verification.notice');
 });
 
 Route::group(['controller' => SessionsController::class], function () {
-    Route::get('login', 'create')->middleware('guest')->name('login.create');
+    Route::view('login', 'auth.login')->middleware('guest')->name('login.create');
     Route::post('login', 'store')->middleware('guest')->name('login.store');
     Route::get('logout', 'destroy')->middleware('auth')->name('logout.destroy');
 });
 
 Route::group(['controller' => ForgotPasswordController::class, 'middleware' => 'guest', 'prefix' => '/forgot-password'], function () {
-    Route::get('/', 'email')->name('recover.email');
+    Route::view('/', 'auth.verifyPassword.email-confirm')->name('recover.email');
     Route::post('/', 'send')->name('recover.send');
     Route::view('/recover/verify', 'auth.verify')->name('recover.password.message');
     Route::get('/recover/{token}', 'password')->name('recover.password.create');
     Route::post('/reset-password', 'reset')->name('recover.password.reset');
 });
 
-Route::group([], function () {
-    Route::view('email-verified', 'auth.verified', 
-        ['message' => 'messages.confirm_acc', 'redirect' => 'logout.destroy']
-    )->middleware(['auth', 'verified'])->name('email.verified');
+Route::view('email-verified', 'auth.verified', 
+    ['message' => 'messages.confirm_acc', 'redirect' => 'logout.destroy']
+)->middleware(['auth', 'verified'])->name('email.verified');
 
-    Route::view('recover-verified', 'auth.verified', 
-        ['message' => 'messages.updated_pass', 'redirect' => 'login.create']
-    )->middleware('guest')->name('recover.verified');
-});
+Route::view('recover-verified', 'auth.verified', 
+    ['message' => 'messages.updated_pass', 'redirect' => 'login.create']
+)->middleware('guest')->name('recover.verified');
 
 Route::group(['controller' => DashboardController::class, 'middleware' => 'auth', 'prefix' => '/dashboard'], function() {
     Route::get('/world-wide', 'index')->name('dashboard.index');
