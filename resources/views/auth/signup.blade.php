f9dd40e760e6f7a3adc28b427be6b4609cb5c910
jarji <100996174+jarjia@users.noreply.github.com>
2023-05-02 17:29:39 +0400

    chore: making app responsive

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -3,7 +3,7 @@
         <h2 class='text-xl text-[#010414] mb-1 2xl:mb-2 font-bold sm:text-2xl 2xl:text-[30px]'>{{__('forms.welcome_to_corona_time')}}</h2>
         <h3 class='text-md text-[#808189] tracking-wide text-gray sm:text-lg 2xl:text-[24px]'>{{__('forms.register_desc')}}</h3>
     </div>
-    <form method='POST' action="{{route('signup.store')}}" class='w-[420px] 2xl:w-[460px] 2xl:mt-8 sm:w-full sm:pr-2' novalidate>
+    <form method='POST' action="{{route('signup.store')}}" class='w-[420px] 2xl:w-[460px] 2xl:mt-0 sm:w-full sm:pr-2' novalidate>
         @csrf
 
         <x-input :name="'name'" :type="'text'" :label="__('inputs.username')" :placeholder="__('inputs.username_placeholder')"/>
--- resources/views/components/layout-for-auth.blade.php
+++ resources/views/components/layout-for-auth.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <section class='grid grid-cols-session sm:flex sm:w-full sm:gap-2 w-full h-full'>
-            <section class='px-[90px] 2xl:px-[170px] 2xl:py-16 py-8 sm:px-4 sm:w-full sm:py-2'>
+            <section class='pl-20 pr-16 2xl:px-[170px] pt-1 minHeight:pt-4 pt-0 sm:px-4 sm:w-full sm:py-2'>
                 <div class='mb-6 flex items-center justify-between w-[420px] 2xl:w-[460px] sm:w-full'>
                     <div>
                         <img src='/images/group-1.png' alt='app logo' class='sm:w-[137px] 2xl:w-[200px]'/>
